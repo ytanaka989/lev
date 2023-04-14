f3093fae436d498fc67c9a453c31efdc1014dba4
ytanaka989 <22011212yt@tama.ac.jp>
2023-04-14 13:32:57 +0900

    1ページ目

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Live</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        
+    </head>
+    <body class="antialiased">
+       <h1>お笑いレビューサイト</h1>
+       <div class='posts'>
+           <div class='post'>
+               <h2 class='title'>Title1</h2>
+           </div>
+       </div>
+    </body>
+</html>
